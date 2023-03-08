5acc83d302a9fb1c255e493989619314fb2a732c
kmr22live <60565922+kmr22live@users.noreply.github.com>
2023-03-08 19:08:37 +0530

    updated the table in html

--- pagination_DOM/script.js
+++ pagination_DOM/script.js
@@ -1,20 +1,71 @@
 let heading1 = document.createElement("h1");
-heading1.setAttribute("id", "heading1");
+heading1.setAttribute("id", "title");
 heading1.textContent = "Pagination DOM Task";
 document.body.append(heading1);
 
+let p = document.createElement("p");
+p.setAttribute("id", "description");
+p.textContent = "Using HTML, CSS and JS";
+document.body.append(p);
+
 let div1 = document.createElement("div");
 div1.setAttribute("id", "div1");
-div1.textContent = "hai";
+div1.setAttribute("class", "table-responsive");
 document.body.append(div1);
 
 let divText = document.getElementById("div1");
+
+let table = document.createElement("table");
+table.setAttribute("class", "table");
+divText.append(table);
+
+let tab = document.querySelector(".table");
+tab.classList.add("table-bordered");
+
+let tr = document.createElement("tr");
+tab.append(tr);
+
+function createtabHEad(heading) {
+  let th = document.createElement("th");
+  th.textContent = heading;
+  tr.append(th);
+}
+
+createtabHEad("Name:");
+createtabHEad(`ID :`);
+createtabHEad("Email:");
+
+let tr1 = document.createElement("tr");
+tab.append(tr1);
+
+function createtabData(heading) {
+  let td = document.createElement("th");
+  td.setAttribute("id", `${heading}`);
+  td.textContent = "Demo";
+  tr1.append(td);
+}
+
+createtabData("name");
+createtabData("idno");
+createtabData("email");
+
+let tableName = document.getElementById("name");
+let tableid = document.getElementById("idno");
+let tableemail = document.getElementById("email");
+
+let divlast = document.createElement("div");
+divlast.setAttribute("id", "buttons");
+divlast.setAttribute("class", "d-flex");
+document.body.append(divlast);
+
+let divPage = document.getElementById("buttons");
+divPage.classList.add("justify-content-center");
 
 let unorderedList = document.createElement("ul");
 unorderedList.setAttribute("id", "ulList");
 unorderedList.setAttribute("class", "pagination");
 unorderedList.setAttribute("data-current-page", "1");
-document.body.append(unorderedList);
+divPage.append(unorderedList);
 
 let unlist = document.getElementById("ulList");
 
@@ -66,17 +117,20 @@ jsonfile.onload = function () {
     let users = JSON.parse(jsonfile.response);
     let usersLen = users.length;
     createlist("First");
+
     createlist("Previous");
     for (let i = 2; i <= usersLen; i++) {
       createlist(i);
     }
     createlist("Next");
+    createlist("Last");
     let pageList = document.querySelectorAll(".page-item");
     console.log(pageList[0]);
     // pageList[0].classList.add("disabled");
     let firstpage = pageList[0];
     let previouspage = pageList[1];
     let nextpage = pageList[usersLen + 1];
+    let lastpage = pageList[usersLen + 2];
 
     let pageinwindow = function (n) {
       pageList.forEach((t) => {
@@ -86,6 +140,7 @@ jsonfile.onload = function () {
       firstpage.classList.remove("d-none");
       previouspage.classList.remove("d-none");
       nextpage.classList.remove("d-none");
+      lastpage.classList.remove("d-none");
 
       allorder.forEach((arr) => {
         if (arr.includes(+n)) {
@@ -99,9 +154,12 @@ jsonfile.onload = function () {
     function showtodiv(n) {
       if (Number.isInteger(+n)) {
         let show = +n - 1;
-        divText.innerHTML = `Name:${users[show].name}<br>
-    ID:${users[show].id}<br>
-    Email:${users[show].email}`;
+        //     divText.innerHTML = `Name:${users[show].name}<br>
+        // ID:${users[show].id}<br>
+        // Email:${users[show].email}`;
+        tableName.textContent = users[show].name;
+        tableid.textContent = users[show].id;
+        tableemail.textContent = users[show].email;
         pageinwindow(n);
         pageList[+n].classList.add("active");
         previouspage.classList.remove("active");
@@ -119,7 +177,13 @@ jsonfile.onload = function () {
       showtodiv(presentpage);
       firstpage.classList.add("active");
     });
+    lastpage.addEventListener("click", function () {
+      presentpage = 100;
+      showtodiv(presentpage);
+      lastpage.classList.add("active");
+    });
     nextpage.addEventListener("click", function () {
+      if (presentpage == 100) lastpage.classList.add("active");
       if (presentpage <= 100) {
         presentpage += 1;
         showtodiv(presentpage);
