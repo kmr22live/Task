d05d7d6ae3807bc3bd143a4b2737ff35f9fdba2c
kmr22live <60565922+kmr22live@users.noreply.github.com>
2023-03-15 18:25:02 +0530

    Update script.js

--- api_tasks/url_shortener/script.js
+++ api_tasks/url_shortener/script.js
@@ -4,6 +4,14 @@ let errormsg = document.getElementById("error-msg");
 let result1 = document.getElementById("result");
 let resultPage = document.getElementById("result-page");
 
+function successfull() {
+  document.body.style =
+    "background-image: url(https://tibushlabs.b-cdn.net/shrtcode/img/confetti.gif)";
+  setTimeout(() => {
+    document.body.style = "background-image: none";
+  }, 5000);
+}
+
 document.getElementById("submit").addEventListener("click", function () {
   if (user_input.value != "") {
     error.classList.add("d-none");
@@ -13,16 +21,19 @@ document.getElementById("submit").addEventListener("click", function () {
       .then((obj) => {
         if (radiobtn == "shrtco.de") {
           result1.classList.remove("d-none");
+          successfull();
           resultPage.textContent = obj.result.short_link;
           resultPage.href = `http://${obj.result.short_link}`;
         }
         if (radiobtn == "9qr.de") {
           result1.classList.remove("d-none");
+          successfull();
           resultPage.textContent = obj.result.short_link2;
           resultPage.href = `http://${obj.result.short_link2}`;
         }
         if (radiobtn == "shiny.link") {
           result1.classList.remove("d-none");
+          successfull();
           resultPage.textContent = obj.result.short_link3;
           resultPage.href = `http://${obj.result.short_link3}`;
         }
