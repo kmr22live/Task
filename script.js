1d866d458c77e6d098fe1acbd0a3e1cdfadadfcb
Mohanraj Kathirvel <60565922+kmr22live@users.noreply.github.com>
2023-06-20 18:07:47 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-// Do the below programs in anonymous function & IIFE
+// Do the below programs in anonymous function & IIFE.
 //-----------------------------------------------------------------------
 
 //a. Print odd numbers in an array.
